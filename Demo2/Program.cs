759ad95d5b28fcec40d477fddbd2f97c2e1151a4
Tobias Ansey <tobias.ansey@outlook.com>
2019-12-05 13:52:43 +0100

    Tobi wollte eine ocmmitmessage

--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Gtec.Unicorn;
 
 namespace UnicornNetAcquisitionExample
@@ -8,6 +9,21 @@ namespace UnicornNetAcquisitionExample
         static void Main(string[] args)
         {
             Console.WriteLine("API Version: " + Unicorn.GetApiVersion());
+
+            Unicorn _device = new Unicorn("UN-2019.02.86");
+            uint FrameLength = 1;
+            uint numberOfAcquiredChannels = _device.GetNumberOfAcquiredChannels();
+            byte[] receiveBuffer = new byte[FrameLength * sizeof(float) * numberOfAcquiredChannels];
+            GCHandle receiveBufferHandle = GCHandle.Alloc(receiveBuffer, GCHandleType.Pinned);
+            _device.StartAcquisition(false);
+
+            while (true)
+            {
+                _device.GetData(FrameLength, receiveBufferHandle.AddrOfPinnedObject(), (uint)(receiveBuffer.Length / sizeof(float)));
+
+
+            }
+
             Console.ReadLine();
         }
     }
