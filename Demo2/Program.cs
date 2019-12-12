16cc6eed96ead1a9e77fab0a0afa5ab4240e8c3f
Tobias Ansey <tobias.ansey@outlook.com>
2019-12-12 13:54:55 +0100

    commited fabicode der verderbnis

--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -6,6 +6,9 @@ namespace UnicornNetAcquisitionExample
 {
     class Program
     {
+
+        string helper = "";
+      
         static void Main(string[] args)
         {
             Console.WriteLine("API Version: " + Unicorn.GetApiVersion());
@@ -17,12 +20,29 @@ namespace UnicornNetAcquisitionExample
             GCHandle receiveBufferHandle = GCHandle.Alloc(receiveBuffer, GCHandleType.Pinned);
             _device.StartAcquisition(false);
 
-            while (true)
+            //while(true)
+            for (int i = 0; i < 100; i++)
             {
                 _device.GetData(FrameLength, receiveBufferHandle.AddrOfPinnedObject(), (uint)(receiveBuffer.Length / sizeof(float)));
+               for(int k = 0; k < 17; k++)
+                {
+                    byte[] tmp = new byte[4];
+                    for( int j = 4 * k + 0; j < 4*k + 4; j++)
+                    {
+                        tmp[j % 4] = receiveBuffer[j];
+                    }
+                    float result = BitConverter.ToSingle(tmp, 0);
+                    Console.Write( result + " " );
 
 
+                }
+                Console.WriteLine();
+                Console.WriteLine();
             }
+
+            Console.WriteLine();
+
+            _device.StopAcquisition();
 
             Console.ReadLine();
         }
